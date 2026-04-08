f4b13e72c915caa54a5e59180a4e817989816247
Amirul Azam <amirulazam115@gmail.com>
2026-04-08 19:01:01 +0600

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Banner from "./Component/Banner"
+import Footer from "./Component/Footer"
 import FourthBanner from "./Component/FourthBanner"
 import NavBar from "./Component/NavBar"
 import SecondBanner from "./Component/SecondBanner"
@@ -17,6 +18,7 @@ function App() {
        <SecondBanner></SecondBanner>
        <ThirdBanner></ThirdBanner>
        <FourthBanner></FourthBanner>
+       <Footer></Footer>
     </>
 
   )
--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,7 +3,46 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            
+<footer>
+
+  <div className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 pr-0 pl-20">
+    <nav>
+      <h1 className="text-3xl font-bold">DigiTools</h1>
+      <p className="font-">Premium digital tools for creators, <br /> professionals, and businesses. Work smarter <br /> with our suite of powerful tools.</p>
+  </nav>              
+  <nav>
+    <h6 className="footer-title">Services</h6>
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Company</h6>
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Legal</h6>
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+
+<nav>
+    <p>Social Links</p>
+    <div className="space-x-2">
+        <button><i class="fa-brands fa-facebook"></i></button>
+        <button><i class="fa-brands fa-instagram"></i></button>
+        <button><i class="fa-brands fa-x-twitter"></i></button>
+    </div>
+</nav>
+  </div>
+  
+</footer>
+  
         </div>
     );
 };
